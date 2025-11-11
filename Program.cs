84a4a2b9416920b640f5c09612687579b7891f07
l3miage-diallom6 <mamad.aliou2001@gmail.com>
2025-11-11 18:50:13 +0100

    Debut partie 3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using CabinetInfirmier;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        await XMLUtils.ValidateXmlFileAsync(
+            "http://www.univ-grenoble-alpes.fr/l3miage/medical",
+            "data/xsd/cabinet.xsd",
+            "data/xml/cabinet.xml"
+        );
+    }
+}
---
+++ XMLUtils.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+
+namespace CabinetInfirmier;
+
+public static class  XMLUtils {
+    public static async Task ValidateXmlFileAsync(string schemaNamespace, string xsdFilePath, string xmlFilePath)
+    {
+        var settings = new XmlReaderSettings();
+        settings.Schemas.Add(schemaNamespace, xsdFilePath);
+        settings.Schemas.Add("http://www.univ-grenoble-alpes.fr/l3miage/actes", "data/xsd/actes.xsd");
+        settings.ValidationType = ValidationType.Schema;
+        Console.WriteLine("Nombre de schemas utilisés dans la validation : " + settings.Schemas.Count);
+        settings.ValidationEventHandler += ValidationCallBack;
+        var readItems = XmlReader.Create(xmlFilePath, settings);
+        while (readItems.Read()) { }
+    }
+
+    private static void ValidationCallBack(object? sender, ValidationEventArgs e)
+    {
+        if (e.Severity.Equals(XmlSeverityType.Warning))
+        {
+            Console.Write("WARNING : ");
+            Console.WriteLine(e.Message);
+        }
+        else if (e.Severity.Equals(XmlSeverityType.Error))
+        {
+            Console.Write("ERROR : ");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
