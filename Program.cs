0daad163bc6856c63642941d70dd220c668a655d
l3miage-diallom6 <mamad.aliou2001@gmail.com>
2025-11-11 20:17:03 +0100

    Transformation xslt avec C#

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CabinetInfirmier;
+using System;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
 
 class Program
 {
@@ -8,10 +12,21 @@ class Program
     {
         Console.WriteLine("Hello, World!");
 
-        await XMLUtils.ValidateXmlFileAsync(
+        /*await XMLUtils.ValidateXmlFileAsync(
             "http://www.univ-grenoble-alpes.fr/l3miage/medical",
             "data/xsd/cabinet.xsd",
             "data/xml/cabinet.xml"
-        );
+        );*/
+        
+        /*await XMLUtils.ValidateXmlFileAsync(
+            "http://www.univ-grenoble-alpes.fr/l3miage/actes",
+            "data/xsd/actes.xsd",
+            "data/xml/actes.xml"
+        );*/
+        
+        XMLUtils.XslTransform(
+            "../../../data/xml/cabinet.xml", 
+            "../../../data/xslt/infirmier.xslt", 
+            "../../../data/html/infirmier.html");
     }
 }
